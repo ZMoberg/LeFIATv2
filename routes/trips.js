249948a6818ff66b8ae39690b96487cbdb0c9c74
Zack Moberg <zmobergca@gmail.com>
2021-11-21 11:23:19 -0800

    trips

--- routes/trips.js
+++ routes/trips.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('trips')
+    res.render('trips/index')
 })
+
+router.get('/new', (req, res) => {
+    res.render('trips/new')
+})
+
+
 
 module.exports = router
