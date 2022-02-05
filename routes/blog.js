20927b08bda20576f98f66934ca3377771d9daa5
Zack Moberg <zmobergca@gmail.com>
2022-02-05 09:59:45 -0800

    clouds carousel

--- routes/blog.js
+++ routes/blog.js
@@ -73,6 +73,28 @@ router.delete('/:id', async (req, res) => {
     res.redirect('/blog')
 })
 
+// function saveArticleAndRedirect(path) {
+    
+//     return async (req, res) => {
+//         let article = req.article
+//         article.title = req.body.title
+//         article.description = req.body.description
+//         article.author = req.body.author
+//         article.markdown = req.body.markdown
+//         article.image = req.file.path
+//     try {
+//         if (! req.file || ! req.file.path) {
+//             return res.sendStatus(400);
+//           }
+//         article = await article.save()
+//         res.redirect(`/blog/${article.slug}`)
+//     } catch(err) {  
+//         console.log(err)
+//         res.render(`blog/${path}`, { article: article })
+//     }      
+//     }
+// }
+
 function saveArticleAndRedirect(path) {
     
     return async (req, res) => {
@@ -83,16 +105,24 @@ function saveArticleAndRedirect(path) {
         article.markdown = req.body.markdown
         article.image = req.file.path
     try {
-        if (! req.file || ! req.file.path) {
+        if (! req.file != null && ! req.file.path !== '') {
             return res.sendStatus(400);
           }
         article = await article.save()
         res.redirect(`/blog/${article.slug}`)
     } catch(err) {  
-        console.log(err)
-        res.render(`blog/${path}`, { article: article })
+        if (article != null) {
+            console.log(err)
+            res.render(`blog/${path}`, { article: article })
+        } else {
+            redirect('/edit')
+        }
+        
     }      
     }
 }
+
+
+
 
 module.exports = router
