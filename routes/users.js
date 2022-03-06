c90619fe774117345a6c72bafb8c779d9f8d7fd6
Zack Moberg <zmobergca@gmail.com>
2022-03-06 09:28:49 -0800

    errors

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,19 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/user')
 const ejsRender = require("../utils/ejsRender");
+const catchAsync = require('../utils/catchAsync')
 
 router.get('/register', (req, res) => {
     ejsRender(req, res, 'users/register')
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', catchAsync(async (req, res) => {
     const {email, username, password} = req.body
     const user = new User({email, username})
     const registeredUser = User.register(user, password)
     console.log(registeredUser)
-    res.redirect('/')
-})
+    res.redirect('/login')
+}))
 
 
 module.exports = router;
