3401f50f970427730c7c429b9779e2a658506cec
Zack Moberg <zmobergca@gmail.com>
2022-03-07 08:06:55 -0800

    flash

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.use((req, res, next ) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
-    console.log()
+    console.log(res.locals.currentUser)
     next()
 })
 
